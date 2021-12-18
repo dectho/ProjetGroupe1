485adc8ce6c02cff92cedb43c96632a276495aa9
Mirko D'Elia <la198895@student.helha.be>
2021-12-18 12:27:54 +0100

    artist

---
+++ src/app/line-up/artist-form/artist-form.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import {FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-artist-form',
+  templateUrl: './artist-form.component.html',
+  styleUrls: ['./artist-form.component.css']
+})
+export class ArtistFormComponent implements OnInit {
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
