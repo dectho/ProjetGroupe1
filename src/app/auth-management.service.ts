237dd7b19f3e68b70b3c57d73adbd7eef69f68cf
Isaac Antoine <ant.olympe@live.be>
2021-12-23 10:27:49 +0100

    Token et tout

---
+++ src/app/auth-management.service.ts
@@ -0,0 +1,43 @@
+import {Injectable, OnInit} from '@angular/core';
+import {environment} from "../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Music} from "./music";
+import {User} from "./user";
+import {Observable} from "rxjs";
+import {EventType} from "./event-bus/event-type";
+import {EventBusService} from "./event-bus/event-bus.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthManagementService implements  OnInit{
+
+  private static readonly API_URL: string = environment.apiUrl + "/AuthManagement";
+
+  token : any;
+
+  constructor(private httpClient : HttpClient, private eventBus : EventBusService) { }
+
+  ngOnInit() {
+    this.eventBus.when(EventType.DISCONNECTED).subscribe(value => this.token = value);
+    this.eventBus.when(EventType.ADMIN_CONNECTED).subscribe(value =>
+    {
+      this.token = value;
+    });
+    this.eventBus.when(EventType.USER_CONNECTED).subscribe(value =>
+    {
+      this.token = value;
+    });
+  }
+
+  login(user : User) : Observable<Request>
+  {
+    return this.httpClient.post<Request>(AuthManagementService.API_URL + "/login", user);
+  }
+
+  register(user : User) : Observable<Request>
+  {
+    return this.httpClient.post<Request>(AuthManagementService.API_URL + "/Register", user);
+  }
+
+}
---
+++ src/app/request.ts
@@ -0,0 +1,5 @@
+export interface Request {
+  token:string;
+  isSuccess:boolean;
+  errors:string[];
+}
